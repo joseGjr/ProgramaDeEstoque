534cf707f344a378bd9c253d25f28b948c6bf28a
joseGjr <jratividades@hotmail.com>
2022-06-22 08:35:40 -0300

    add files

---
+++ src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import Entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+        System.out.print("Enter product data: " );
+        System.out.println("Name:  ");
+        product.name = sc.nextLine();
+
+        System.out.print("Price: " );
+        product.price = sc.nextDouble();
+        System.out.println("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+
+        System.out.println( product.name+", "+ product.price+", "+product.quantity);
+
+        sc.close();
+
+
+
+    }
+}
